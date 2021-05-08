5ec58d451d4f156130f3f07c2290097df9372c3d
tqk2811 <tqk2811@gmail.com>
2021-05-08 15:33:48 +0700

    Add project files.

---
+++ TqkLibrary.Media/FFMPEG/FfprobeHepler.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace TqkLibrary.Media.FFMPEG
+{
+  public class FfprobeHepler
+  {
+    private readonly string ffprobePath;
+
+    public FfprobeHepler(string ffprobePath = "FFMPEG\\ffprobe.exe")
+    {
+      if (!File.Exists(ffprobePath)) throw new FileNotFoundException(ffprobePath);
+      this.ffprobePath = ffprobePath;
+    }
+
+    private string Analyse_Console(string filePath)
+    {
+      string arguments = $"-print_format json -show_format -show_streams \"{filePath}\"";//-show_format -sexagesimal
+      ProcessStartInfo processStartInfo = new ProcessStartInfo(ffprobePath);
+      processStartInfo.Arguments = arguments;
+      processStartInfo.CreateNoWindow = true;
+      processStartInfo.UseShellExecute = false;
+      processStartInfo.RedirectStandardOutput = true;
+      using (Process process = Process.Start(processStartInfo)) return process.StandardOutput.ReadToEnd();
+    }
+
+    public MediaInfo Analyse(string filePath)
+    {
+      string text_json = Analyse_Console(filePath);
+      return JsonConvert.DeserializeObject<MediaInfo>(text_json);
+    }
+  }
+
+  public class MediaStream
+  {
+    public string codec_name { get; set; }
+    public string codec_type { get; set; }
+    public string r_frame_rate { get; set; }
+    public double? duration { get; set; }
+    public int? width { get; set; }
+    public int? height { get; set; }
+    public int? bit_rate { get; set; }
+    public int? sample_rate { get; set; }
+
+    public override string ToString()
+    {
+      return $"{codec_type} {codec_name}";
+    }
+  }
+
+  public class MediaFormat
+  {
+    public double? duration { get; set; }
+    public int? bit_rate { get; set; }
+
+    public override string ToString()
+    {
+      return $"duration: {duration}, bit_rate: {bit_rate}";
+    }
+  }
+
+  public class MediaInfo
+  {
+    public List<MediaStream> streams { get; set; }
+    public MediaFormat format { get; set; }
+  }
+}
---
+++ TqkLibrary.Media/Sounds/ReadMp3Length/ReadMp3Length.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace TqkLibrary.Media.Sounds
+{
+  public static class ReadMp3Duration
+  {
+    public static TimeSpan Read(Stream stream, int length)
+    {
+      var id3v2Tag = Id3v2Tag.ReadTag(stream);
+      long dataStartPosition = stream.Position;
+      var firstFrame = Mp3Frame.LoadFromStream(stream);
+      if (firstFrame == null)
+        throw new InvalidDataException("Invalid MP3 file - no MP3 Frames Detected");
+      double bitRate = firstFrame.BitRate;
+      var xingHeader = XingHeader.LoadXingHeader(firstFrame);
+      // If the header exists, we can skip over it when decoding the rest of the file
+      if (xingHeader != null) dataStartPosition = stream.Position;
+
+      // workaround for a longstanding issue with some files failing to load
+      // because they report a spurious sample rate change
+      var secondFrame = Mp3Frame.LoadFromStream(stream);
+      if (secondFrame != null &&
+          (secondFrame.SampleRate != firstFrame.SampleRate ||
+           secondFrame.ChannelMode != firstFrame.ChannelMode))
+      {
+        // assume that the first frame was some kind of VBR/LAME header that we failed to recognise properly
+        dataStartPosition = secondFrame.FileOffset;
+        // forget about the first frame, the second one is the first one we really care about
+        firstFrame = secondFrame;
+      }
+
+      long mp3DataLength = length - dataStartPosition - 128;
+      double totaltime = mp3DataLength * 8.0 / firstFrame.BitRate;
+      return TimeSpan.FromSeconds(totaltime);
+    }
+  }
+}
